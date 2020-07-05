c717970e73a68dbf0dc60e1b029cf636d801ddbd
Rob Daley <robd@itaboi.co.uk>
2020-07-05 22:04:36 +0100

    Scripted database deploy and initial data load in docker environment scripts.

--- ItaBank.Environment/enviro.ps1
+++ ItaBank.Environment/enviro.ps1
@@ -6,6 +6,11 @@ if ($action -eq "start") {
 	cd docker
 	docker-compose up -d
 	cd ..
+	write-output "Wait 15 seconds to ensure containers have spun up before proceeding..."
+	sleep 15
+	msbuild ..\ItaBank.Database\ItaBank.Database.sqlproj /p:Configuration=Release
+	SqlPackage.exe /a:Publish /sf:"..\ItaBank.Database\bin\Release\ItaBank.Database.dacpac" /tcs:"Server=lunatic;Database=ItaBank;User Id=sa;Password=b11a8fe75d7866ee3fB1f871589493123;"
+	sqlcmd -S localhost -U sa -P b11a8fe75d7866ee3fB1f871589493123 -Q "exec ItaBank.dbo.LoadDummyData;"
 }
 elseif ($action -eq "stop") {
 	cd docker
