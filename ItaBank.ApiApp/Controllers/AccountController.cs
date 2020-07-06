146338066a65a6d6a601ea603dd8d72566a59f4b
Rob Daley <robd@itaboi.co.uk>
2020-07-06 14:41:37 +0100

    Initial implementation of ApiApp project.

---
+++ ItaBank.ApiApp/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using ItaBank.SDK.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ItaBank.ApiApp.Controllers
+{
+    [ApiController]
+    [Route("api/account")]
+    public class AccountController : Controller
+    {
+        public AccountController()
+        {
+        }
+
+        [HttpGet]
+        public ActionResult<List<Account>> Get()
+        {
+            var accountsMapped = new List<Account>()
+            {
+                new Account
+                {
+                    Id = 1,
+                    Code = "AC0001",
+                    Name = "Mr Rob Daley",
+                    Address = "10 Downing Street, Westminster, London, EC1 1LV",
+                    Balance = 25000
+                }
+            };
+
+            return accountsMapped;
+        }
+    }
+}
